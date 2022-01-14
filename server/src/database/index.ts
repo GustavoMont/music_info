81b9e07ec7f8c555298cb148345d4c1ba8291e69
GustavoMont <lgmluisgm@gmail.com>
2022-01-14 11:47:48 -0300

    :pencil: create db connection

---
+++ server/src/database/index.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+const db = new Sequelize({
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  dialect: "mysql",
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+});
+
+export default db;
