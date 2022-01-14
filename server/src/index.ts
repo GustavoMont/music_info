6390f84c60c405a602970e7c240645644c274e74
GustavoMont <lgmluisgm@gmail.com>
2022-01-14 11:48:34 -0300

    :pencil: connect and sync my db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
-import express from 'express'
-import 'dotenv/config'
+import express from "express";
+import "dotenv/config";
+import db from "./database";
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+(async () => {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
-
-app.listen(PORT, () =>{console.log("Server is runing");})
+app.listen(PORT, () => {
+  console.log("Server is runing");
+});
